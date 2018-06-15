b6899438e4aa4ba53e45efc95d0a94ebd0ccd468
angelica winkler <caw400@nyu.edu>
2018-06-15 15:55:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(x)
     return "FizzBuzz"
   elsif x % 5 == 0
     return "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0
-    return "FizzBuzz"
+  elsif x % 3 == 0
+    return "Fizz"
   end
 end
